7fcd475b89377eb9206c1f5bd5421f90b22b73b9
Jay Patel <jnpatel2811@gmail.com>
2018-10-14 12:28:11 +0530

    Added page view and 4 pages on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,21 +8,52 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF128C7E),
-        title: Text("Whatsapp", textAlign: TextAlign.left,),
-        centerTitle: false,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.search),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF128C7E),
+          title: Text("Whatsapp", textAlign: TextAlign.left,),
+          centerTitle: false,
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.search),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.more_vert),
+            ),
+          ],
+          bottom: TabBar(tabs: [
+            Tab(child: Icon(Icons.camera_alt),),
+            Tab(child: Text("Chats"),),
+            Tab(child: Text("Status"),),
+            Tab(child: Text("Calls"),),
+          ]),
+        ),
+        body: TabBarView(children: [
+          Column(
+            children: <Widget>[
+              Text("Camera"),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.more_vert),
+          Column(
+            children: <Widget>[
+              Text("Chats"),
+            ],
           ),
-        ],
+          Column(
+            children: <Widget>[
+              Text("Stories"),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text("Calls"),
+            ],
+          ),
+        ]),
       ),
     );
   }
