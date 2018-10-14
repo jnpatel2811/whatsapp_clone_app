c96f81a5a10ae369414ac91527654d87cda31d88
Jay Patel <jnpatel2811@gmail.com>
2018-10-14 13:47:33 +0530

    Added list view to show chats with dummy data

---
+++ lib/UserData.dart
@@ -0,0 +1,6 @@
+class UserData {
+  String name;
+  String message;
+
+  UserData(this.name, this.message);
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_app/UserData.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,14 +7,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var demoChatData = [
+    UserData("Jay", "Hey there!"),
+    UserData("Nilay", "Flutter is so cool!"),
+    UserData("Sagar", "Yeah! Nilay is right."),
+    UserData("Jaydeep", "I don't know about it."),
+    UserData("Maulik", "Bro, this is like magic. It is so cool.."),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
+      initialIndex: 1,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xFF128C7E),
-          title: Text("Whatsapp", textAlign: TextAlign.left,),
+          title: Text(
+            "Whatsapp",
+            textAlign: TextAlign.left,
+          ),
           centerTitle: false,
           actions: <Widget>[
             Padding(
@@ -26,10 +39,18 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
           bottom: TabBar(tabs: [
-            Tab(child: Icon(Icons.camera_alt),),
-            Tab(child: Text("Chats"),),
-            Tab(child: Text("Status"),),
-            Tab(child: Text("Calls"),),
+            Tab(
+              child: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              child: Text("Chats"),
+            ),
+            Tab(
+              child: Text("Status"),
+            ),
+            Tab(
+              child: Text("Calls"),
+            ),
           ]),
         ),
         body: TabBarView(children: [
@@ -44,17 +65,57 @@ class _HomePageState extends State<HomePage> {
 
   Widget _buildCameraLayout() {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("Camera"),
+        Icon(Icons.camera_alt),
       ],
     );
   }
 
   Widget _buildChatsLayout() {
-    return Column(
-      children: <Widget>[
-        Text("Chats"),
-      ],
+    return ListView.builder(
+      itemCount: demoChatData.length,
+      itemBuilder: (context, position) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 48.0,
+                    ),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    flex: 7,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            demoChatData[position].name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 4.0),
+                            child: Text(demoChatData[position].message,
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+        );
+      },
     );
   }
 
